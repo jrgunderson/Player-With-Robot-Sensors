ce0dbd8be99f3e99071a34c67ff9fd94235c2cbc
shpadoinklejoe <shpadoinklejoe@yahoo.com>
2017-10-06 16:22:48 -0700

    this project does not need stage

---
+++ Player for Ubuntu 14.04/src/install_player.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# make sure to run with super user (sudo) privileges
+# restart terminal after running this script
+
+##### Install Player-Stage #####
+
+# install svn and git
+sudo apt-get -y install subversion
+sudo apt-get -y install git
+
+# build player
+cd player/
+mkdir build
+cd build/
+
+sudo cmake ../
+sudo make
+sudo make install
+
+cd ../..
+
+# add additional path to environment variable to be able to run "player /usr/local/share/stage/worlds/simple.cfg"
+arch=$(uname -m)
+if [ "$arch" == "i686" ]
+then 
+	sudo sed -i '$ a\export LD_LIBRARY_PATH+=/usr/local/lib' /etc/bash.bashrc
+	sudo sed -i '$ a\export STAGEPATH+=/usr/local/lib' /etc/bash.bashrc
+	sudo sed -i '$ a\export PLAYERPATH+=/usr/local/lib' /etc/bash.bashrc
+fi
+if [ "$arch" == "x86_64" ]
+then
+	sudo sed -i '$ a\export LD_LIBRARY_PATH+=/usr/local/lib64' /etc/bash.bashrc
+        sudo sed -i '$ a\export STAGEPATH+=/usr/local/lib64' /etc/bash.bashrc
+        sudo sed -i '$ a\export PLAYERPATH+=/usr/local/lib64' /etc/bash.bashrc
+fi
+
--- Player-Stage for Ubuntu 14.04/src/player/build/utils/pmap/pmapconfig.h
+++
@@ -1,2 +0,0 @@
-#define OPENGL_FOUND 1
-#define GLUT_FOUND 1
