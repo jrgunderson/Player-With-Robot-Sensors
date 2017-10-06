5c3f4e21efed0b3651a25c1df9e95c1d24b700f6
shpadoinklejoe <shpadoinklejoe@yahoo.com>
2017-10-06 15:15:03 -0700

    no errors when compiling, but thread not working for some reason

--- Qt Projects/Left Robot Code/Communicator/main.cpp
+++ Qt Projects/Left Robot Code/Communicator/main.cpp
@@ -8,14 +8,16 @@ using namespace std;
 
 Driver *d;
 
+void *PrintHello(void*);
+
 // function to be executed by thread
-void PrintHello(void *threadid)
+void *PrintHello(void* id)
 {
     // since incoming argument must be void type
     // cast back to non-void type
-    int info = (int)threadid;
+    long info = (long)id;
 
-   cout << "Hello World! Thread ID, " << info << endl;
+   cout << "Hello World! Thread ID, "<< id << endl;
 
    // close thread
    pthread_exit(NULL);
@@ -24,13 +26,13 @@ void PrintHello(void *threadid)
 int main(int argc, char *argv[])
 {
     // only 1 thread
-    pthread_t threads[1];
+    pthread_t thread;
 
     // info to be passed
     int info = 1;
 
     // paraemters = (which thread, NULL=default, function thread is executing, pointer variable (NULL=no arguments))
-    pthread_create(&threads[1], NULL, PrintHello, (void *)info);
+    pthread_create(&thread, NULL, PrintHello, (void *)info);
 
 
     Locate();
